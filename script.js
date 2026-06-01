5c21400fdb525b385e4504e1aadac395b34a8eaa
Ndm115 <motala.nadeem@gmail.com>
2026-06-01 22:38:13 +0200

    Updated interface and styling

--- script.js
+++ script.js
@@ -6,6 +6,32 @@ function goToUpload() {
     window.location.href = "upload.html";
 }
 
-function goToResults() {
-    window.location.href = "results.html";
+function goToSummary() {
+    window.location.href = "summary.html";
 }
+
+function goToQuiz() {
+    window.location.href = "quiz.html";
+}
+
+function goToTutor() {
+    window.location.href = "tutor.html";
+}
+
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+window.onload = function () {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+};
